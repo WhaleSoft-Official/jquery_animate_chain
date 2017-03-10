9d98e7d1dfd7745448f3cdba31c4c6aeafd9b65f
whalesoft-repo <George@mfc.dp.ua>
2017-03-10 15:32:47 +0200

    Fixed multiply call on one page. Fixed chaining

--- animation_chane.js
+++ animation_chane.js
@@ -4,17 +4,27 @@
         animationTime: 1000,
         direction : 'down',
         delay: 0
-	};
+	},
+	$animation_collection_id = 0;
+	
     $.fn.animateChane = function(options){
         var settings = $.extend({}, defaults, options),
             $collection = this,
             $element = this;
-            
-        if($collection.length > 1) $element = $($collection[0]);
+        if($collection.length <= 0) return this;    
+        if($collection.length > 1){
+            $collection.attr('data-animation_collection_id', ++$animation_collection_id)
+            $element = $($collection[0]);  
+        }else{
+            var current_animation_collection_id = $element.data('animation_collection_id')
+            $collection = $('[data-animation_collection_id="'+current_animation_collection_id+'"]');
+        } 
+        
+        var current_element_index = $collection.index($element);
         $element.animate(settings.animate, settings.animationTime, function(){
             $element.trigger('chane_element_animation_finished')
-            var next_element = $element.next();
-            if(settings.direction == 'up') next_element = $element.prev();
+            var next_element = $($collection[current_element_index+1]);
+            if(settings.direction == 'up') next_element = $($collection[current_element_index-1]);
             if(next_element.length > 0){
                 setTimeout(function(){
                     next_element.animateChane(options);
@@ -22,5 +32,7 @@
             }
             else $element.trigger('chane_animation_finished');
         })
+        
+        return this;
     }
 }(window.jQuery);
